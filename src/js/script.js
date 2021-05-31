7d3ee896f78e2354094c24767b6e7efec87b16e0
saket2001 <saketchandorkar123@gmail.com>
2021-05-31 12:47:26 +0530

    menu testing

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,13 @@
-// // fetch(
-// //   `https://cors-anywhere.herokuapp.com/https://api.documenu.com/v2/restaurant/4072702673999819?key=a748d3cae6d1fa9976cc8a1e701e72d9`
-// // )
-// //   .then((res) => res.json())
-// //   .then((data) => {
-// //     const show = data.result.restaurant_name;
-// //     document.querySelector(".show").textContent = show;
-// //     console.log(data);
-// //   })
-// //   .catch((err) => console.log(err));
+// fetch(
+//   `https://cors-anywhere.herokuapp.com/https://api.documenu.com/v2/restaurant/4072702673999819?key=a748d3cae6d1fa9976cc8a1e701e72d9`
+// )
+//   .then((res) => res.json())
+//   .then((data) => {
+//     // const show = data.result.restaurant_name;
+//     // document.querySelector(".").textContent = show;
+//     console.log(data.result.menus[0].menu_sections);
+//   })
+//   .catch((err) => console.log(err));
 
 // document
 //   .querySelector("#cart__container")

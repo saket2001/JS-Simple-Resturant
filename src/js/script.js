368ee9e0428d7ff0962a06d54d7a6e46ffe534d1
saket2001 <saketchandorkar123@gmail.com>
2021-05-31 16:51:48 +0530

    did js menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,67 @@
-// fetch(
-//   `https://cors-anywhere.herokuapp.com/https://api.documenu.com/v2/restaurant/4072702673999819?key=a748d3cae6d1fa9976cc8a1e701e72d9`
-// )
-//   .then((res) => res.json())
-//   .then((data) => {
-//     // const show = data.result.restaurant_name;
-//     // document.querySelector(".").textContent = show;
-//     console.log(data.result.menus[0].menu_sections);
-//   })
-//   .catch((err) => console.log(err));
+const menuContainer = document.querySelector("#menu__container");
 
-// document
-//   .querySelector("#cart__container")
-//   .addEventListener("click", function (e) {
-//     const btn = e.target.closest(".cart__btn");
-//     alert(btn);
+////////////////////////////////////
 
-//     const markup = `
-//         <div id="cart__div">
-//             Your cart is
-//         </div>
-//       `;
+const getData = async function () {
+  try {
+    const res = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.documenu.com/v2/restaurant/4072702673999819?key=a748d3cae6d1fa9976cc8a1e701e72d9`
+    );
 
-//     document.querySelector("header").classList.toggle("overlay");
+    if (!res) return;
 
-//     document.querySelector("body").insertAdjacentHTML("afterbegin", markup);
-//   });
+    const data = await res.json();
+
+    // console.log(data.result.menus[0].menu_sections);
+    await renderMenu(data.result.menus[0].menu_sections);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// render menu data
+const renderMenu = function (data) {
+  data.forEach((row) => {
+    // console.log(row.menu_items);
+    const markup = `
+            <details>
+                <summary>
+                   ${row.section_name}
+                </summary>
+                <div id="menu__list">
+                ${row.menu_items
+                  .map((item) => {
+                    return `
+                    <div class="menu__item">
+                        <div class="menu__left">
+                            <div class="menu__title">
+                                ${item.name}
+                            </div>
+                            <div class="menu__description">
+                                  ${
+                                    item.description
+                                      ? item.description
+                                      : "No Description available"
+                                  }
+                            </div>
+                            <button class="btn">
+                                Add to cart
+                            </button>
+                        </div>
+                        <div class="menu__right">
+                            ${item.price}$
+                        </div>
+                    </div>
+                  `;
+                  })
+                  .join("")}
+                </div>  
+            </details> 
+    `;
+
+    menuContainer.insertAdjacentHTML("beforeend", markup);
+  });
+};
+
+////////////////////////////////
+window.addEventListener("load", getData);

33e9b6689460e19440fdd9da06dc4888e1e6b419
saket2001 <saketchandorkar123@gmail.com>
2021-06-04 10:30:02 +0530

    fixed subtotal bug

--- src/js/controller.js
+++ src/js/controller.js
@@ -79,6 +79,9 @@ const cancelItem = function (e) {
 
   if (model.state.cart.length === 0)
     document.querySelector(".cart_total p").innerHTML = "";
+  cartBtn.querySelector(".cart__btn").src =
+    "./src/images and icons/icons8-shopping-cart.svg";
+  cartBtn.querySelector(".cart__btn").style.width = "25px";
 
   if (model.state.cart.length > 0) {
     init();
